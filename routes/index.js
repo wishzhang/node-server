19f261f2cc2e597f7d7d533169b0941ca61c1c9a
1535703141@qq.com <1535703141@qq.com>
2019-11-22 18:27:59 +0800

    启动

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var multiparty = require('multiparty');
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.post('/file', function (req, res, next) {
+    var form = new multiparty.Form();
+    // form.encoding = 'utf-8';
+    form.uploadDir = 'public/uploads/';
+
+    form.parse(req,function (err,fields,files) {
+        debugger
+        res.send('hihi');
+    })
 });
 
 module.exports = router;
